99a25127b29c2be38c67794aae9fe0fc322791d5
sezeef <sezeef.h@gmail.com>
2024-09-04 13:52:55 +0300

    Impl footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,3 +1,16 @@
+import Link from "next/link";
+
 export function Footer() {
-  return <div></div>;
+  return (
+    <footer className="w-full h-10 px-8 flex justify-between items-center bg-yellow-50/70 text-accent-foreground">
+      <p>All rights reserved ©2024</p>
+      <Link
+        href="https://github.com/sezeef/crop-recommendation"
+        target="_blank"
+        className="underline"
+      >
+        Github
+      </Link>
+    </footer>
+  );
 }
